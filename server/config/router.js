62833aca5a732284e7d1391d1837b7acccbecb1b
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-08 22:00:19 +0100

    all endpoints hit correctly; BE MVP reached

--- server/config/router.js
+++ server/config/router.js
@@ -5,13 +5,13 @@ const model = require("../data/db-model");
 const router = express.Router();
 
 router.get("/products", getAllProducts);
-router.get("/products/:id", getProductById);
+router.get("/products/:id", validateProductId, getProductById);
 router.get("/merchants", getAllMerchants);
-router.get("/merchants/:id", getMerchantById);
-router.get("/merchant/:id/products", getProductsByCategory);
+router.get("/merchants/:id", validateMerchantId, getMerchantById);
+router.get("/merchant/:id/products", validateMerchantId, getProductsByCategory);
 
 function getAllProducts(req, res) {
-    model
+  model
     .getAllProducts()
     .then(products => {
       res.json(products);
@@ -21,9 +21,11 @@ function getAllProducts(req, res) {
     });
 }
 
-function getProductById(req, res) {}
+function getProductById(req, res) {
+  res.json(req.product);
+}
 function getAllMerchants(req, res) {
-    model
+  model
     .getAllMerchants()
     .then(merchants => {
       res.json(merchants);
@@ -32,7 +34,63 @@ function getAllMerchants(req, res) {
       res.status(500).json({ message: "Failed to get merchants: " + error });
     });
 }
-function getMerchantById(req, res) {}
-function getProductsByCategory(req, res) {}
+function getMerchantById(req, res) {
+  res.json(req.merchant);
+}
+function getProductsByCategory(req, res) {
+  const id = req.params.id;
+  model
+    .getProductsByCategory(id)
+    .then(products => {
+      res.json(products);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Failed to get products: " + error });
+    });
+}
 
+//Middleware
+function validateProductId(req, res, next) {
+  const id = req.params.id;
+  model
+    .getProductById(id)
+    .then(product => {
+      if (product) {
+        req.product = product;
+        next();
+      } else {
+        res
+          .status(404)
+          .json({
+            message: "product id does not correspond with an actual product"
+          });
+      }
+    })
+    .catch(error => {
+      res.status(404).json({ message: "invalid product id: " + error.message });
+    });
+}
+
+function validateMerchantId(req, res, next) {
+  const id = req.params.id;
+  model
+    .getMerchantById(id)
+    .then(merchant => {
+      if (merchant) {
+        req.merchant = merchant;
+        next();
+      } else {
+        res
+          .status(404)
+          .json({
+            message: "merchant id does not correspond with an actual merchant"
+          });
+      }
+    })
+    .catch(error => {
+      res
+        .status(404)
+        .json({ message: "invalid merchant id: " + error.message });
+    });
+}
 module.exports = router;
