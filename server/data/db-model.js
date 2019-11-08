c1e62f71e5fd82b32c4701dcfffc27a1ceb6c817
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-08 21:59:53 +0100

    model completed

--- server/data/db-model.js
+++ server/data/db-model.js
@@ -13,14 +13,31 @@ function getAllProducts() {
    return db('products')
 }
 function getProductById(product_id) {
-
+    return db("products").where({ product_id: Number(product_id) }).first()
 }
+
+
 function getAllMerchants() {
  return db('merchants')
 }
 function getMerchantById(merchant_id) {
-
+    return db("merchants").where({ merchant_id: Number(merchant_id) }).first()
 }
 function getProductsByCategory(merchant_id) {
+/*
+select
+p.product_name 
+from product_categories as pc
+join merchants as m
+on  pc.merchant_id= m.merchant_id
+join products as p
+on  pc.product_id= p.product_id
+where m.merchant_id = 2
 
+*/
+return db('product_categories as pc')
+.join('merchants as m','pc.merchant_id','m.merchant_id')
+.join('products as p','pc.product_id','p.product_id')
+.select('p.product_name ')
+.where({'m.merchant_id': merchant_id})
 }
