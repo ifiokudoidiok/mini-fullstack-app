f8127eb822e86b327b114acb7f8a719ab5c749f0
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-08 20:36:38 +0100

    knex configured for development

---
+++ knexfile.js
@@ -0,0 +1,25 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true, // needed for sqlite
+    connection: {
+      filename: '../server/data/market.db3',
+    },
+    migrations: {
+      directory: '../server/data/migrations'
+    },
+    seeds: {
+      directory: '../server/data/seeds'
+    },
+    // add the following
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
+    },
+  }, 
+};
