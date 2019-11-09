80e32a3c7d69e74287c6c1e49d6451babe38b442
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-09 21:54:03 +0100

    all end points hit correctly and displayed as expected

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Route } from "react-router-dom";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
+  const [productList, setProductList] = useState([]);
+  const [merchantList, setmerchantList] = useState([]);
+  const [productCategory, setproductCategory] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/api/products")
+      .then(res => {
+        // debugger
+        setProductList(res.data);
+      })
+      .catch(error => {
+        debugger;
+        alert(error.message);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/api/merchants")
+      .then(res => {
+        // debugger
+        setmerchantList(res.data);
+      })
+      .catch(error => {
+        debugger;
+        alert(error.message);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/api/merchant/1/products`)
+      .then(res => {
+        // debuggersetproductCategory
+        setproductCategory(res.data);
+      })
+      .catch(error => {
+        debugger;
+        alert(error.message);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {productList.map(products => (
+        <>
+          <p>Name: {products.product_name}</p>
+          <p>Description: {products.product_description}</p>
+          <p>Price: {products.product_price}</p>
+        </>
+      ))}
+      {merchantList.map(merchants => (
+        <>
+          <p>Name: {merchants.merchant_name}</p>
+        </>
+      ))}
+      {productCategory.map(products => (
+        <>
+          <p>Name: {products.product_name}</p>
+          <p>Description: {products.product_description}</p>
+          <p>Price: {products.product_price}</p>
+        </>
+      ))}
     </div>
   );
 }
